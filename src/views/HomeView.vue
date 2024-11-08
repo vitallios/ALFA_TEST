4ebab125ba0590de292de6f4114c9c7b48a6ced0
Vitallios <61820576+vitallios@users.noreply.github.com>
2024-11-08 16:04:27 +0300

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -71,14 +71,14 @@ const createNewChild = () => {
     <!-- Родитель -->
 
     <div class="parentName flex flex-col ">
-    <h2>Персональные данные</h2>
+      <h2>Персональные данные</h2>
       <div class="pt-[22px]">
         <label for="parentNameLabel">Имя</label>
-      <input v-model="Parent.name" class="w-full border-none outline-none" type="text">
+        <input v-model="Parent.name" class="w-full border-none outline-none" type="text">
       </div>
       <div>
-      <label for="parentAgeLabel">Возраст</label>
-      <input v-model="Parent.age" class="w-full border-none outline-none" type="text">
+        <label for="parentAgeLabel">Возраст</label>
+        <input v-model="Parent.age" class="w-full border-none outline-none" type="text">
       </div>
     </div>
     <!-- Родитель -->
@@ -87,8 +87,7 @@ const createNewChild = () => {
     <div>
       <div class="flex flex-row items-center ">
         <h2>Дети (макс. 5)</h2>
-        <button  @click="createNewChild()" 
-        class="btn flex justify-between items-center">
+        <button @click="createNewChild()" class="btn flex justify-between items-center">
           <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" clip-rule="evenodd"
               d="M5.13332 10.8556C4.50125 10.8555 3.98887 11.3679 3.98887 12C3.98887 12.6321 4.50126 13.1445 5.13332 13.1445L10.8554 13.1445L10.8554 18.8668C10.8554 19.4989 11.3678 20.0113 11.9999 20.0113C12.632 20.0113 13.1444 19.4989 13.1444 18.8668L13.1443 13.1445L18.8667 13.1445C19.4988 13.1445 20.0112 12.6321 20.0112 12C20.0112 11.3679 19.4988 10.8556 18.8667 10.8556L13.1443 10.8556L13.1443 5.13338C13.1443 4.50132 12.632 3.98893 11.9999 3.98893C11.3678 3.98893 10.8554 4.50131 10.8554 5.13338L10.8554 10.8556L5.13332 10.8556Z"
@@ -99,8 +98,16 @@ const createNewChild = () => {
       </div>
       <ul id="ChildrenList" class="pt-[22px]">
         <li class="childrenItem">
-          <input v-model="Child.name" type="text" placeholder="Иван" aria-label="Имя">
-          <input v-model="Child.age" type="text" placeholder="Возраст"  aria-label="Возраст">
+          <div>
+            <label for="childrenNameLabel">Имя</label>
+            <input v-model="Child.name" type="text" placeholder="Иван" aria-label="Имя">
+          </div>
+          <div>
+            <label for="childrenAgeLabel">Возраст</label>
+            <input v-model="Child.age" type="text" placeholder="Возраст" aria-label="Возраст">
+
+          </div>
+
           <button class="btn removeBtn">Удалить</button>
         </li>
       </ul>
@@ -112,7 +119,8 @@ const createNewChild = () => {
 </template>
 
 <style scoped>
-.parentName>div>input {
+.parentName>div>input,
+.childrenItem>div>input {
   padding: 7px 16px;
   border-radius: 8px;
   border: 1px solid #DDE2E5;
@@ -123,15 +131,18 @@ const createNewChild = () => {
   line-height: 24px;
   font-size: 14px;
 }
-.parentName>div{
+
+.parentName>div,
+.childrenItem>div {
   position: relative;
 }
-.parentName>div>label {
+
+.parentName>div>label,
+.childrenItem>div>label {
   position: absolute;
   padding: 8px 0 2px 16px;
   font-size: 13px;
   color: var(--lightBlack);
   z-index: 10;
 }
-
 </style>
